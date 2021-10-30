66a9f8349526e7e8fedf9506d53ee83990a5499f
Codeoffun <88065232+codeoffun2@users.noreply.github.com>
2021-10-29 20:09:55 -0400

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,28 @@
+const { Client, Intents } = require("discord.js");
+const client = new Client({
+  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+});
+
+// When the app is ready it will write out Ready using this
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (message.content === '!Hello') {
+        // send back "Pong." to the channel the message was sent in
+        message.channel.send('hello to you too.');
+	}
+
+});
+
+client.on('message', (message) => {
+ let regex = /who/i;
+ if (regex.test(message.content)) {
+  message.channel.send("ur mom");
+ }
+
+});
+
+// Replace the value between the quotes with your token
+client.login('redacted for privacy');
